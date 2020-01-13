5491de177e99c1f8fef85871611621a9bc68c354
Kanghae Choi <kanghaechoi@gmail.com>
2020-01-13 15:24:23 -0500

    Git initialization

--- capture.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-server="http://rpi-6.wifi.local.cmu.edu:8888/video_feed"
-VCap=cv2.VideoCapture(server)
-if not VCap.isOpened():
-    print("ERROR!")
-for i in range(1):
-    ret, frame=VCap.read()
-    cv2.imwrite("frame.jpg", frame)
