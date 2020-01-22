77dd94fab786366e28349f92039f6c88fb5a17f3
Kanghae Choi <kanghaechoi@gmail.com>
2020-01-21 19:57:29 -0500

    simplified loading page

--- panhandle_webpage/app/modules/loading.py
+++ panhandle_webpage/app/modules/loading.py
@@ -10,24 +10,38 @@ def loading():
     sql = "SELECT red, blue, green, pending=1 FROM orderdb.orders"
     selected_data = db_select.executeAll(sql)
 
-    try:
-        data_per_3 = selected_data[0:3]
-        print(data_per_3[0]['red'])
-        print(type(data_per_3[0]))
-        print(len(data_per_3))
+    # try:
+    current_data = selected_data[0:3]
+    print(current_data[0]['red'])
+    print(type(current_data[0]))
+    print(len(current_data))
 
-        red_sum = 0
-        green_sum = 0
-        blue_sum = 0
+    current_red_sum = 0
+    current_green_sum = 0
+    current_blue_sum = 0
 
-        for i in range(len(data_per_3)):
-            red_sum += data_per_3[i]['red']
-            green_sum += data_per_3[i]['green']
-            blue_sum += data_per_3[i]['blue']
-    except:
-        data_per_3 = selected_data[0::]
+    for i in range(len(current_data)):
+        current_red_sum += current_data[i]['red']
+        current_green_sum += current_data[i]['green']
+        current_blue_sum += current_data[i]['blue']
+
+    # try:
+    next_data = selected_data[3:6]
+
+    next_red_sum = 0
+    next_green_sum = 0
+    next_blue_sum = 0
+
+    for i in range(len(next_data)):
+        next_red_sum += next_data[i]['red']
+        next_green_sum += next_data[i]['green']
+        next_blue_sum += next_data[i]['blue']
+
 
     return render_template("/pages/loading.html",
-                           current_red=red_sum,
-                           current_green=green_sum,
-                           current_blue=blue_sum)
+                           current_red=current_red_sum,
+                           current_green=current_green_sum,
+                           current_blue=current_blue_sum,
+                           next_red=next_red_sum,
+                           next_green=next_green_sum,
+                           next_blue=next_blue_sum)
